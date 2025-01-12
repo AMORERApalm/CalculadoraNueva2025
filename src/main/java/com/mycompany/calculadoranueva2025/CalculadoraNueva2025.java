7382c6ee84beedf578f67205e2e14d762fd9e2bb
winadmin <winadmin@sgbrwab>
2025-01-12 22:49:11 +0100

    1 Commit

---
+++ src/main/java/com/mycompany/calculadoranueva2025/CalculadoraNueva2025.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoranueva2025;
+
+/**
+ *
+ * @author winadmin
+ */
+public class CalculadoraNueva2025 {
+
+    public static int sumar (int a, int b) 
+            {
+                return a+b;
+            }
+    
+    public static int restar (int a, int b) 
+            {
+                return a-b;
+            }
+    
+}
